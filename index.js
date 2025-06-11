fa0482fb0d841763ff729f97e606ee1bb88cf4e4
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-06-11 13:30:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+const PORT = 3001
+mongoose.connect(process.env.MONGO_URL) // No options needed anymore
+  .then(() => console.log(' Connected to MongoDB'))
+  .catch((err) => console.error(' Connection error:', err.message));
